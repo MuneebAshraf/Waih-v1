21300ffb18b9b1724c34945b035826049155d3c5
backurdi <bachir098@gamil.com>
2017-07-13 11:12:46 +0200

    DB upload fungere
    upload.php fungere, WUUUUHUUUUU

--- PHP/upload.php
+++ PHP/upload.php
@@ -1,38 +1,53 @@
 <?php
-
-if(isset($_POST["save_audio"]) && $_POST["save_audio"]=="Upload audio") {
-
-    $dir = 'audio/';
-
-    $audio_path = $dir . basename($_FILES['audioFile']['name']);
-
-    if (move_uploaded_file($_FILES['audioFile']['tmp_name'], $audio_path)) {
-        echo 'leggo';
-    } else {
-        echo 'kom nuuu';
+// Check if a file has been uploaded
+if(isset($_FILES['uploaded_file'])) {
+    // Make sure the file was sent without errors
+    if($_FILES['uploaded_file']['error'] == 0) {
+        // Connect to the database
+        $dbLink = new mysqli('waih.dk.mysql', 'waih_dk_podcasts', 'waihpodcasts', 'waih_dk_podcasts');
+        if(mysqli_connect_errno()) {
+            die("MySQL connection failed: ". mysqli_connect_error());
+        }
+ 
+        // Gather all required data
+        $name = $dbLink->real_escape_string($_FILES['uploaded_file']['name']);
+        $mime = $dbLink->real_escape_string($_FILES['uploaded_file']['type']);
+        $data = $dbLink->real_escape_string(file_get_contents($_FILES  ['uploaded_file']['tmp_name']));
+        $size = intval($_FILES['uploaded_file']['size']);
+ 
+        // Create the SQL query
+        $query = "
+            INSERT INTO `myTable` (
+                `name`, `mime`, `size`, `data`, `created`
+            )
+            VALUES (
+                '{$name}', '{$mime}', {$size}, '{$data}', NOW()
+            )";
+ 
+        // Execute the query
+        $result = $dbLink->query($query);
+ 
+        // Check if it was successfull
+        if($result) {
+            echo 'Success! Your file was successfully added!';
+        }
+        else {
+            echo 'Error! Failed to insert the file'
+               . "<pre>{$dbLink->error}</pre>";
+        }
     }
-} else {
-    echo "Filen er ikke fundet";
+    else {
+        echo 'An error accured while the file was being uploaded. '
+           . 'Error code: '. intval($_FILES['uploaded_file']['error']);
+    }
+ 
+    // Close the mysql connection
+    $dbLink->close();
 }
-function saveAudio($fileName)
-    {
-        $conn=mysqli_connect('waih.dk.mysql','waih_dk_podcasts','waihpodcasts','waih_dk_podcasts');
-                if(!$conn)
-                {
-                    die('Du er uduelig');
-                }
-
-                $query="insert into Podcasts('Title')values('{$fileName}')";
-
-                mysqli_query($conn,$query);
-
-                if(mysqli_affected_rows($conn)>0)
-                {
-                echo "alhamdulillah";
-                }
-
-                mysqli_close($conn);
-
-    }
-
+else {
+    echo 'Error! A file was not sent!';
+}
+ 
+// Echo a link back to the main page
+echo '<p>Click <a href="index1.php">here</a> to go back</p>';
 ?>
--- js/programsTEST.js
+++ js/programsTEST.js
@@ -15,17 +15,17 @@ var stateObj = { programmer: "programmer" };
 
 window.onpopstate = function(event) {
 };
-addPodcasts("17-04-2017","Vært: Johannes<br>Gæster: Omar El-khatib & Abu Yunes<br>Dato: 17/04-2017");
-addPodcasts("10-04-2017","Vært: Johannes<br>Gæster: Engin Bozkir & Elias Rama<br>Dato: 10/04-2017");
-addPodcasts("03-04-2017","Vært: Johannes<br>Gæster: Elias Rama<br>Dato: 02/04-2017");
-addPodcasts("27-03-2017","Vært: Johannes<br>Gæster: Engin Bozkir & Elias Rama<br>Dato: 27/03-2017");
-addPodcasts("20-03-2017","Vært: Johannes<br>Gæster: Engin Bozkir & Abu Yunes<br>Dato: 20/03-2017");
-addPodcasts("13-03-2017","Vært: Johannes<br>Gæst: Engin Bozkir<br>Dato: 13/03-2017");
-addPodcasts("06-03-2017","Vært: Johannes & Abu Yunes<br>Gæst: Engin Bozkir<br>Dato: 06/03-2017");
-addPodcasts("27:02-2017","Vært: Elias Rama<br>Gæst: Lamies Nassri<br>Dato: 27/02-2017");
-addPodcasts("20:02-2017","Vært: Johannes<br>Gæst:  Tarek Ghanoum<br>Dato: 20/02-2017");
-addPodcasts("13:02-2017","Vært: Johannes<br>Gæst: Abdullahi Hassan<br>Dato: 13/02-2017");
-addPodcasts("06:02-2017","Vært: Elias Rama<br>Gæst: Tarek Ghanoum<br>Dato: 06/02-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes<br>Gæster: Omar El-khatib & Abu Yunes<br>Dato: 17/04-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes<br>Gæster: Engin Bozkir & Elias Rama<br>Dato: 10/04-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes<br>Gæster: Elias Rama<br>Dato: 02/04-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes<br>Gæster: Engin Bozkir & Elias Rama<br>Dato: 27/03-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes<br>Gæster: Engin Bozkir & Abu Yunes<br>Dato: 20/03-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes<br>Gæst: Engin Bozkir<br>Dato: 13/03-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes & Abu Yunes<br>Gæst: Engin Bozkir<br>Dato: 06/03-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Elias Rama<br>Gæst: Lamies Nassri<br>Dato: 27/02-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes<br>Gæst:  Tarek Ghanoum<br>Dato: 20/02-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Johannes<br>Gæst: Abdullahi Hassan<br>Dato: 13/02-2017");
+addPodcasts("https://soundcloud.com/bachir-kurdi/apropos-med-elias-rama","Vært: Elias Rama<br>Gæst: Tarek Ghanoum<br>Dato: 06/02-2017");
 
 
 function isbjerg() {
